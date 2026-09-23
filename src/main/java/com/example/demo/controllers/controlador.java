67a58ee0e871d035672428d47d43f9d44c456b84
Abel Osco <est.abeldaniel.osco.aq@unifranz.edu.bo>
2026-09-23 09:10:35 -0400

    Actualización de código y ajustes

--- src/main/java/com/example/demo/controllers/controlador.java
+++ src/main/java/com/example/demo/controllers/controlador.java
@@ -6,18 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/pila")
 public class controlador {
-
-    // Instancia de tu pila para almacenar cadenas (o el tipo de dato que prefieras)
     private final pila<String> miPila = new pila<>();
-
-    // 1. Agregar elemento: POST http://localhost:8080/api/pila/agregar?valor=Hola
    @GetMapping ("/agregar")
     public String agregar(@RequestParam String valor) {
         miPila.agregar(valor);
         return "Elemento '" + valor + "' agregado. Tamaño actual: " + miPila.size();
     }
-
-    // 2. Quitar elemento: DELETE http://localhost:8080/api/pila/quitar
     @GetMapping("/quitar")
     public String quitar() {
         String eliminado = miPila.quitar();
@@ -26,14 +20,10 @@ public class controlador {
         }
         return "La pila está vacía.";
     }
-
-    // 3. Obtener tamaño: GET http://localhost:8080/api/pila/size
     @GetMapping("/size")
     public int size() {
         return miPila.size();
     }
-
-    // 4. Mostrar en consola del servidor: GET http://localhost:8080/api/pila/mostrar
     @GetMapping("/mostrar")
     public String mostrar() {
         miPila.mostrar();
